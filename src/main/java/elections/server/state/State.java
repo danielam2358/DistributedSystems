3cd675ba1af343e363984591b92b1ab0866790e5
danielam2358 <danielamram953@gmail.com>
2019-12-19 12:36:17 +0200

    succeed init sever

--- src/main/java/elections/server/state/State.java
+++ src/main/java/elections/server/state/State.java
@@ -1,5 +1,20 @@
 package elections.server.state;
 
+import elections.client.voter.VoterController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+
 public class State {
 
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(RSocketProperties.Server.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
 }
+
