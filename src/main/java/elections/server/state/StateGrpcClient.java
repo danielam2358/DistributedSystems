29220d9ee41eaee12a8c464057153cd48f55c664
danielam2358 <danielamram953@gmail.com>
2019-12-24 12:24:52 +0200

    update generate grpc code

--- src/main/java/elections/server/state/StateGrpcClient.java
+++ src/main/java/elections/server/state/StateGrpcClient.java
@@ -3,6 +3,71 @@ package elections.server.state;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import server.state.BallotGrpc;
+import server.state.StateGrpcProto;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class StateGrpcClient {
+    private static final Logger logger = Logger.getLogger(StateGrpcClient.class.getName());
+
+    private ManagedChannel channel;
+    private BallotGrpc.BallotBlockingStub blockingStub;
+
+    /** Construct client connecting to HelloWorld server at {@code host:port}. */
+    public StateGrpcClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build());
+    }
+
+    /** Construct client for accessing HelloWorld server using the existing channel. */
+    StateGrpcClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = BallotGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+        StateGrpcProto.VoteRequest request = StateGrpcProto.VoteRequest.newBuilder().setName(name).build();
+        StateGrpcProto.VoteReply response;
+        try {
+            response = blockingStub.vote(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Greeting: " + response.getStatus());
+    }
+
+    // TODO: finish
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the
+     * greeting.
+     */
+    public static void main(String[] args) throws Exception {
+        // Access a service running on the local machine on port 50051
+        StateGrpcClient client = new StateGrpcClient("localhost", 50051);
+        try {
+            String user = "world";
+            // Use the arg as the name to greet if provided
+            if (args.length > 0) {
+                user = args[0];
+            }
+            client.greet(user);
+        } finally {
+            client.shutdown();
+        }
+    }
+
 }
--- src/main/java/elections/server/state/StateGrpcServer.java
+++ src/main/java/elections/server/state/StateGrpcServer.java
@@ -2,12 +2,80 @@ package elections.server.state;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import server.state.BallotGrpc;
+import server.state.StateGrpcProto;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public class StateGrpcServer {
     private static final Logger logger = Logger.getLogger(StateGrpcServer.class.getName());
     private Server server;
+
+
+    // BallotImpl
+    static class BallotImpl extends BallotGrpc.BallotImplBase {
+
+        @Override
+        public void vote(StateGrpcProto.VoteRequest req, StreamObserver<StateGrpcProto.VoteReply> responseObserver) {
+            StateGrpcProto.VoteReply reply = StateGrpcProto.VoteReply.newBuilder().build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+
+
+    private void start(int port) throws IOException {
+
+        server = ServerBuilder.forPort(port)
+                .addService(new BallotImpl())
+                .build()
+                .start();
+
+        logger.info("Server started, listening on " + port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    StateGrpcServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() throws InterruptedException {
+        if (server != null) {
+            server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    /**
+     * Main launches the server from the command line.
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final  StateGrpcServer server = new StateGrpcServer();
+        int port = 50051;  // TODO: how should i find port?
+        server.start(port);
+        server.blockUntilShutdown();
+    }
 
 }
